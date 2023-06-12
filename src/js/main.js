696ea0a5239d86be320d5853a1066b69ebd4cf09
SuelenGalhardo <suelenranucci@gmail.com>
2023-06-12 16:36:38 +0200

    added event

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,86 @@
 'use strict';
 
-const ListElement = document.querySelector('.js__list');
-//const InputSelection = document.querySelector('.js__selection');
-//const InputBtn = document.querySelector('.js__submit');
-
-const serverURL = `https://api.disneyapi.dev/character?page=50`;
-
-//const GITHUB_USER = '<SuelenGalhardo>';
+const listElement = document.querySelector('.js__list');
+const ulFavorites = document.querySelector('.js__list__Favorites');
+const inputSelection = document.querySelector('.js__selection');
+const inputBtn = document.querySelector('.js__submit');
+const serverURL = `https://api.disneyapi.dev/character?`;
 
 let animesDataList = [];
+
 //peticion al servidor
+//storage
 //FETCH
-/* */
+
 fetch(serverURL)
   .then((response) => response.json())
   .then((data) => {
     animesDataList = data.data;
-    renderListCharacters(animesDataList);
+    renderListCharacters(animesDataList, listElement);
   });
 
-//FUNCIONS listado de tarjeta
-function renderListCharacters(list) {
-  for (const eachCharacter of list) {
-    ListElement.innerHTML += renderOnlyCharacter(eachCharacter);
+//FUNCIONS
+function renderListCharacters(listData) {
+  listElement.innerHTML = '';
+  for (const Characters of listData) {
+    listElement.innerHTML += renderOnlyCharacter(Characters);
+  }
+  addEventAnimes();
+}
+
+function addEventAnimes() {
+  const liElementList = document.querySelectorAll('.js__li__animes');
+  for (const li of liElementList) {
+    li.addEventListener('click', handleClick);
   }
 }
 
 function renderOnlyCharacter(dataObjeto) {
-  let html = `<li id="${dataObjeto._id}" class="character__card">
+  let html = `<li id="${dataObjeto._id}" class="character__card js__li__animes">
                  <img class="character__img js_img" src="${dataObjeto.imageUrl}" alt="Disney Characters" />
                 <p class="character__name js_name">${dataObjeto.name}</p>
                  
               </li>`;
-  const imgValue = dataObjeto.imageUrl;
-  const witeImg = 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
-  if (imgValue === '') {
+
+  if (dataObjeto.imageUrl === undefined) {
+    const witeImg = 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
     html = `<li id="${dataObjeto._id}"class="character__card">
-                                <img  class="character__img js_img src="${witeImg}" alt="Imagen en Blanco"" />
+                                <img class="character__img js_img" src="${witeImg}" alt="Imagen en Blanco" />
                                 <p class="character__name js_name">${dataObjeto.name}</p>
                         </li>`;
   }
   return html;
 }
+let animesFavorites = [];
 
-//Eventos
-/*linkNewFormElememt.addEventListener('click', handleClickNewCatForm);
-searchButton.addEventListener('click', filterKitten);
-buttonAdd.addEventListener('click', addNewKitten);
-buttonCancelForm.addEventListener('click', cancelNewKitten);*/
+function handleClick(event) {
+  const id = parseInt(event.currentTarget.id);
+  const selectedAnimes = animesDataList.find((item) => item._id === id);
+  const indexAnimes = animesFavorites.findIndex((item) => item._id === id);
+  if (indexAnimes === -1) {
+    animesFavorites.push(selectedAnimes);
+  } else {
+    animesFavorites.splice(indexAnimes, 1);
+  }
+  // guardar favoritos en LS
+  renderListFavorites();
+}
+
+//Funcion para renderizar la lista de favoritos
+function renderListFavorites() {
+  ulFavorites.innerHTML = '';
+
+  for (const Animesfav of animesFavorites) {
+    ulFavorites.innerHTML += renderOnlyCharacter(Animesfav);
+  }
+}
+
+const handleSearch = (event) => {
+  event.preventDefault();
+  const inputValue = inputSelection.value;
+  const filterList = animesDataList.filter((item) =>
+    item.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  renderListCharacters(filterList);
+};
+inputBtn.addEventListener('click', handleSearch);
